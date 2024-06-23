7b03828eb60864830400f80c536a8b3ef6e0bf97
Renan Garcia <renanrgarcia@gmail.com>
2024-06-23 09:10:42 -0300

    section 6 done

---
+++ _classNotes/section6-ControlStructures.md
@@ -0,0 +1,51 @@
+# If structure 
+``` php
+if (condition) {
+  // code
+}
+```
+
+# Nested if structure
+``` php
+if (condition) {
+  if (condition) {
+    // code 1
+  }
+  // code 2
+} 
+```
+
+# If/Else structure
+``` php
+if (condition) {
+  // code
+} else {
+  // code
+}
+```
+
+# Else If structure (Different conditions)
+``` php
+if (condition) {
+  // code
+} elseif (condition) {
+  // code
+} else {
+  // code
+}
+```
+
+# Switch structure
+``` php
+switch ($variable) {
+  case $value1:
+    // code
+    break;
+  case $value2:
+    // code
+    break;
+  default:
+    // code
+    break;
+}
+```
---
+++ section6/exercise24/index.php
@@ -0,0 +1,38 @@
+<?php 
+  $a = "bla bla";
+  $b = 25;
+  $c = "test";
+  $d = 1.5;
+  $e = true;
+
+  if (is_int($a)){
+    echo $a . "is an integer <br>";
+  } else {
+    echo $a . "is not an integer <br>";
+  }
+
+  if (is_int($b)){
+    echo $b . "is an integer <br>";
+  } else {
+    echo $b . "is not an integer <br>";
+  }
+
+  if (is_int($c)){
+    echo $c . "is an integer <br>";
+  } else {
+    echo $c . "is not an integer <br>";
+  }
+
+  if (is_int($d)){
+    echo $d . "is an integer <br>";
+  } else {
+    echo $d . "is not an integer <br>";
+  }
+  
+  if (is_int($e)){
+    echo $e . "is an integer <br>";
+  } else {
+    echo $e . "is not an integer <br>";
+  }
+
+?>
