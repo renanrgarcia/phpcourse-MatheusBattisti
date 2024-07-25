22af43b3491ce3e7c27436f7a22cc49ce4f56b13
renanrgarcia <renanrgarcia@gmail.com>
2024-07-25 06:04:03 -0300

    section17 - Created database and main table on index.php

--- section17-contact-agenda/index.php
+++ section17-contact-agenda/index.php
@@ -2,9 +2,50 @@
 include_once("templates/header.php");
 ?>
 
+<div class="container">
 
-<h1>Contact agenda</h1>
-<i class="fas fa-eye"></i>
+  <?php if (isset($printMsg) && $printMsg != '') : ?>
+  <p id="msg"><?= $printMsg ?></p>
+  <?php endif; ?>
+  <h1 id="main-title">My Agenda</h1>
+
+  <?php if (count($contacts) > 0) : ?>
+  <table class="table" id="contacts-table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Phone</th>
+        <th scope="col">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($contacts as $contact) : ?>
+      <tr>
+        <td scope="row"><?= $contact["id"] ?></td>
+        <td><?= $contact["name"] ?></td>
+        <td><?= $contact["phone"] ?></td>
+        <td class="actions">
+          <a href="<?= $BASE_URL ?>show.php?id=<?= $contact["id"] ?>">
+            <i class="fa-solid fa-eye check-icon"></i>
+          </a>
+          <a href="<?= $BASE_URL ?>edit.php?id=<?= $contact["id"] ?>">
+            <i class="fa-solid fa-pencil edit-icon"></i>
+          </a>
+          <a href="<?= $BASE_URL ?>config/process.php?delete=<?= $contact["id"] ?>">
+            <i class="fa-solid fa-trash delete-icon" data-id="<?= $contact["id"] ?>"></i>
+          </a>
+        </td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+
+  </table>
+  <?php else : ?>
+  <p id="empty-list-text">Your contact list is empty. <a href="<?= $BASE_URL ?>create.php">Add now</a></p>
+
+  <?php endif; ?>
+</div>
 
 <?php
 include_once("templates/footer.php");
--- section17-contact-agenda/templates/header.php
+++ section17-contact-agenda/templates/header.php
@@ -1,6 +1,13 @@
 <?php
 include_once("config/url.php");
 include_once("config/connection.php");
+include_once("config/process.php");
+
+// Clean message
+if (isset($_SESSION['msg'])) {
+  $printMsg = $_SESSION['msg'];
+  $_SESSION['msg'] = '';
+}
 
 ?>
 <!DOCTYPE html>
@@ -11,9 +18,13 @@ include_once("config/connection.php");
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Contact agenda</title>
   <!-- Bootstrap -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css"
+    integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Font-awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+    integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- CSS -->
   <link rel="stylesheet" href="<?php $BASE_URL ?>css/styles.css">
 </head>
