6f45309169bfaeb004b3bdc0097e4ace64062238
renanrgarcia <renanrgarcia@gmail.com>
2024-07-26 07:35:39 -0300

    section17 - Created create.php

--- section17-contact-agenda/config/process.php
+++ section17-contact-agenda/config/process.php
@@ -4,33 +4,65 @@ session_start();
 include_once("connection.php");
 include_once("url.php");
 
+$data = $_POST;
 
-$id;
+if (!empty($data)) {
 
-if (!empty($_GET)) {
-  $id = $_GET["id"];
-}
+  if ($data["type"] === "create") {
+    $name = $data["name"];
+    $phone = $data["phone"];
+    $observations = $data["observations"];
 
-// Get one contact
-if (!empty($id)) {
-  $query = "SELECT * FROM contacts WHERE id = :id";
+    $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)";
 
-  $stmt = $conn->prepare($query);
+    $stmt = $conn->prepare($query);
 
-  $stmt->bindParam(":id", $id);
+    $stmt->bindParam(":name", $name);
+    $stmt->bindParam(":phone", $phone);
+    $stmt->bindParam(":observations", $observations);
 
-  $stmt->execute();
-
-  $contact = $stmt->fetch();
+    try {
+      $stmt->execute();
+      $_SESSION["msg"] = "Contact created successfully!";
+    } catch (PDOException $e) {
+      $error = $e->getMessage();
+      echo "Error: $error";
+    }
+  }
+  // Redirect home
+  header("Location:" . $BASE_URL . "/../index.php");
+  // Data selection
 } else {
-  // Get all contacts
-  $query = "SELECT * FROM contacts";
+  $id;
 
-  $contacts = [];
+  if (!empty($_GET)) {
+    $id = $_GET["id"];
+  }
 
-  $stmt = $conn->prepare($query);
+  // Get one contact
+  if (!empty($id)) {
+    $query = "SELECT * FROM contacts WHERE id = :id";
 
-  $stmt->execute();
+    $stmt = $conn->prepare($query);
 
-  $contacts = $stmt->fetchAll();
+    $stmt->bindParam(":id", $id);
+
+    $stmt->execute();
+
+    $contact = $stmt->fetch();
+  } else {
+    // Get all contacts
+    $query = "SELECT * FROM contacts";
+
+    $contacts = [];
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->execute();
+
+    $contacts = $stmt->fetchAll();
+  }
 }
+
+// Close connection
+$conn = null;
--- section17-contact-agenda/create.php
+++ section17-contact-agenda/create.php
@@ -1,10 +1,30 @@
 <?php
 include_once("templates/header.php");
 ?>
-
-
-<h1>Creating contact</h1>
-
-<?php
-include_once("templates/footer.php");
-?>
+<div class="container">
+  <?php include_once("templates/backbtn.html"); ?>
+  <h1 id="main-title">Create contact</h1>
+  <?php if (isset($printMsg) && $printMsg != '') : ?>
+  <p id="msg"><?= $printMsg ?></p>
+  <?php endif; ?>
+  <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
+    <input type="hidden" name="type" value="create">
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" class="form-control" id="name" name="name" placeholder="Enter name" value="<?= $name ?? '' ?>"
+        required>
+    </div>
+    <div class="form-group">
+      <label for="phone">Phone</label>
+      <input type="text" class="form-control" id="phone" name="phone" placeholder="Enter phone"
+        value="<?= $phone ?? '' ?>">
+    </div>
+    <div class="form-group">
+      <label for="observations">Observations</label>
+      <textarea class="form-control" name="observations" id="observations" cols="30" rows="10"
+        placeholder="Enter observations"><?= $observations ?? '' ?></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    <?php
+    include_once("templates/footer.php");
+    ?>
